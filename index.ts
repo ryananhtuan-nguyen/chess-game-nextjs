74e3e161012113d60451c95ebb06a012a4d3f7a7
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-18 10:03:20 +1300

    based class

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-export class ChessPiece {
-  constructor(protected color: string);
+export abstract class ChessPiece {
+  constructor(
+    protected color: string,
+    protected role: string,
+    protected imageUrl: string,
+    protected coordinate: { rowIdx: number; colIdx: number }
+  ) {}
+
+  setImageUrl(): string {
+    return this.imageUrl;
+  }
+
+  getCoordinate(): { rowIdx: number; colIdx: number } {
+    return this.coordinate;
+  }
 }
