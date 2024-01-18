0db5492ea14f33a0b83fe8d5511e26ff6f5a9668
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-18 18:30:43 +1300

    styling: displaying board

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { ChessBoard } from '@/components/chess-board';
 import { initialBoard } from '@/lib/game-logics/initialBoard';
 export default function Home() {
+  console.log(
+    initialBoard.flat().map((item, idx) => ({
+      ...item,
+      color: idx % 2 == 0 ? 'black' : 'white',
+    }))
+  );
   return (
     <main>
       <div className="flex flex-col items-center justify-center w-full h-full">
