10ea88387112f5f810dbc0fb58856a12e93f54d5
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-19 10:25:27 +1300

    chore: initial board

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { ChessBoard } from '@/components/chess-board';
 export default function Home() {
   return (
-    <main>
+    <main className="h-full">
       <div className="flex flex-col items-center justify-center w-full h-full">
-        <h1>ChessGame</h1>
+        <h1 className="font-bold text-2xl pb-2">ChessGame</h1>
         <ChessBoard />
       </div>
     </main>
