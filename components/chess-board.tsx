fc9c9c1e53e58eb1fd2ea0c9c9bebba2b56d63f3
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-18 21:39:11 +1300

    chore: initial board

--- components/chess-board.tsx
+++ components/chess-board.tsx
@@ -1,28 +1,12 @@
 'use client';
-import { King } from '@/lib/game-logics';
-import { ChessTile } from '@/lib/game-logics/types';
+import { initialBoard } from '@/lib/game-logics/initialBoard';
+import { ChessTileWithColor } from '@/lib/game-logics/types';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-interface ChessBoardProps {
-  initialBoard: ChessTile[][];
-}
-
-export const ChessBoard = ({ initialBoard }: ChessBoardProps) => {
-  const [board, setBoard] = useState<ChessTile[][]>(
-    initialBoard.map((item) =>
-      item.map((tile) => {
-        if (tile.id == '7 4') {
-          return { ...tile, chessPiece: new King('white', { x: 7, y: 4 }) };
-        } else if (tile.id == '0 4') {
-          return { ...tile, chessPiece: new King('black', { x: 0, y: 4 }) };
-        } else {
-          return tile;
-        }
-      })
-    )
-  );
+export const ChessBoard = () => {
+  const [board, setBoard] = useState<ChessTileWithColor[][]>(initialBoard);
 
   return (
     <div className="grid grid-cols-8 grid-flow-row border-2 border-black w-[800px] h-[800px]">
@@ -31,13 +15,11 @@ export const ChessBoard = ({ initialBoard }: ChessBoardProps) => {
         return row.map((tile, tileIdx) => (
           <div
             key={tile.id}
-            className={cn('border-2 border-black w-[100px] h-[100px]', {
-              'bg-black text-white':
-                (isOdd && tileIdx % 2 == 0) || (!isOdd && tileIdx % 2 !== 0),
-              'bg-white text-black': !isOdd && tileIdx % 2 !== 0,
-            })}
+            className={cn(
+              'border-2 border-black w-[100px] h-[100px] bg-opacity-70',
+              `bg-${tile.color}`
+            )}
           >
-            {tile.id}
             {tile.chessPiece && (
               <Image
                 src={tile.chessPiece.getImageUrl()}
