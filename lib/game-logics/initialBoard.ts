e96c07b5af86799c3be17e0d688539002f72d039
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-18 21:39:51 +1300

    fix: board tiles color

--- lib/game-logics/initialBoard.ts
+++ lib/game-logics/initialBoard.ts
@@ -1,4 +1,6 @@
-import { ChessTile } from './types';
+import { Knight, Rook } from '.';
+import { insertPiece } from './helper';
+import { ChessColor, ChessTile, ChessTileWithColor } from './types';
 
 const createBoard = (): ChessTile[][] => {
   const rows = Array.from({ length: 8 }).fill('');
@@ -16,4 +18,22 @@ const createBoard = (): ChessTile[][] => {
   return board;
 };
 
-export const initialBoard = createBoard();
+const newBoard = createBoard();
+
+const boardWithColor: ChessTileWithColor[][] = newBoard.map((row, rowIdx) => {
+  if (rowIdx % 2 == 0) {
+    return row.map((tile, colIdx) => ({
+      ...tile,
+      color: colIdx % 2 == 0 ? 'white' : 'black',
+    }));
+  } else {
+    return row.map((tile, colIdx) => ({
+      ...tile,
+      color: colIdx % 2 == 0 ? 'black' : 'white',
+    }));
+  }
+});
+
+export const initialBoard = boardWithColor.map((item) =>
+  item.map((tile) => insertPiece(tile))
+);
